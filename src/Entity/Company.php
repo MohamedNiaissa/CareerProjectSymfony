20cc6855630d8230c5008a2bcbfb6eda77436244
Mohamed <mohameddrame907@gmail.com>
2022-11-09 09:43:13 +0100

    wip fix create company issues

--- src/Entity/Company.php
+++ src/Entity/Company.php
@@ -16,7 +16,7 @@ class Company
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
+    #[ORM\Column(nullable: true)]
     private ?string $logo = null;
 
     #[ORM\Column(length: 255, nullable: true)]
