705be2d7460a117b998c5280949cd6a5dce23190
Mohamed <mohameddrame907@gmail.com>
2022-11-10 11:34:26 +0100

    can create a job offer

--- src/Form/Type/JobType.php
+++ src/Form/Type/JobType.php
@@ -2,8 +2,14 @@
 
 namespace App\Form\Type;
 
+use App\Entity\Company;
 use App\Entity\Job;
+use Doctrine\ORM\Mapping\Entity;
+use phpDocumentor\Reflection\Type;
+use phpDocumentor\Reflection\Types\Integer;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -12,19 +18,19 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class JobType extends AbstractType
 {
-	public function buildForm(FormBuilderInterface $builder, array $options): void
-	{
-		$builder
-			->add('title', TextType::class)
-			->add('description', TextareaType::class)
-			->add('save', SubmitType::class)
-		;
-	}
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('company', EntityType::class, ["class" => Company::class, "choice_label" => "name"])
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('save', SubmitType::class);
+    }
 
-	public function configureOptions(OptionsResolver $resolver): void
-	{
-		$resolver->setDefaults([
-			'data_class' => Job::class,
-		]);
-	}
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Job::class,
+        ]);
+    }
 }
