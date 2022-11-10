5dfaff118473acd5558df0eab9034abc049c42f6
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2022-11-10 14:53:01 +0100

    Pending template logic, it's not working (#34)

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -6,9 +6,11 @@ use App\Entity\Company;
 use App\Entity\User;
 use App\Form\Type\CompanyType;
 use App\Repository\CompanyRepository;
+use App\Repository\JobRepository;
 use App\Repository\UserRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -36,9 +38,33 @@ class CompanyController extends AbstractController
 
 		$allCompanies = $companyRepository->findAll();
 
-		return $this->renderForm('company/company.html.twig', [
+		return $this->renderForm('company/profile.html.twig', [
 			'form' => $form,
 			'companies' => $allCompanies
 		]);
+	}
+
+	#[Route('/companies/pending', name: 'app_company_jobs')]
+	public function pendingJobs(JobRepository $jobRepository): Response
+	{
+		/** @var \App\Entity\User $user */
+		$user = $this->getUser();
+
+		$allJobs = $jobRepository->findBy(array("company" => $user->getCompany(), "status" => "pending"));
+
+		return $this->renderForm('company/pending.html.twig', [
+			'jobs' => $allJobs,
+		]);
+	}
+
+	#[Route('/companies/pending-accept', name: 'accept_candidate')]
+	public function pendingAcceptCandidate(): Response
+	{
+		/** @var \App\Entity\User $user */
+		$user = $this->getUser();
+
+		return new RedirectResponse(
+			$this->router->generate('app_company_jobs')
+		);
 	}
 }
